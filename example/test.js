52b8e9dd88e2a1e436ee9f13e9b1086ff7907b68
jaumard <Jimmy.Aumard@nestle.com>
2017-11-05 21:35:58 +0100

    add brightness capabilities for idle mode, update sonus

--- example/test.js
+++ example/test.js
@@ -1,9 +1,14 @@
 const LISA = require('../index')
 
 const lisa = new LISA({
-  matrix: '192.168.1.26',//or false to disable the support (false by default)
+  matrix: {
+    ip: '192.168.1.26',
+    idleMode: {
+      brightness: 0.2
+    }
+  },//or false to disable the support (false by default)
     url: 'http://localhost:3000',
-  speaker: require('lisa-speaker-polly'),
+  speaker: null, //require('lisa-speaker-polly'),
     language: 'fr-FR',
     gSpeech: './speech/LISA-gfile.json',
     hotwords: [{ file: './speech/hey_lisa.pmdl', hotword: 'hey lisa' }]
